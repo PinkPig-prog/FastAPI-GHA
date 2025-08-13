0e91c5a60943b3c0063f9729657a6c4ff6396ab3
PinkPig-prog <pigp54146@gmail.com>
2025-08-13 16:08:58 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,18 +1,19 @@
 import json
 import numpy as np
 
-# Pretend trained coefficients
 coef = 2.5
 intercept = 1.0
 
 with open("input.json", "r") as f:
     data = json.load(f)
-    if isinstance(data["x_values"], str):
-        x_values = [float(x) for x in data["x_values"].split(",")]
-    else:
-        x_values = data["x_values"]
 
-x = np.array(data["x"]) 
+# Ensure we get a list of floats
+if isinstance(data["x_values"], str):
+    x_values = [float(x) for x in data["x_values"].split(",")]
+else:
+    x_values = [float(x) for x in data["x_values"]]
+
+x = np.array(x_values)
 y_pred = coef * x + intercept
 
 output = {"predictions": y_pred.tolist()}
